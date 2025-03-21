9f54b55349d4e63926aa699768cb40614b2c0375
josephelk <162306823+josephelk@users.noreply.github.com>
2025-03-21 13:01:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ st.info("This app will predict your obesity level!")
 
 dff = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv") 
 df = pd.DataFrame(dff)
+
+# Expander to show raw data
+with st.expander("Data"):
+    st.write("This is a raw data")
+    st.dataframe(df)
